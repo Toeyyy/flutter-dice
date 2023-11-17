1606ed3240a90bfd31af3dbf68c4acca8bb1d086
Toeyyy <baitoeyatikarn@gmail.com>
2023-11-17 22:10:23 +0700

    แปะ

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNum = Random().nextInt(6) + 1;
+      rightDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              changeDiceFace();
+            },
+            child: Image.asset('images/dice$leftDiceNum.png'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNum.png')),
+        ),
+      ]),
+    );
   }
 }
